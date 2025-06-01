38d6f2c4e9292395903f62a2b87784a419e89b10
noushad970 <ug2102038@cse.pstu.ac.bd>
2025-06-02 06:41:06 +0900

    completed posting system

---
+++ Assets/Scripts/UIs/PanelManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    public GameObject homePanel, forYouPanel, PostAQuestionPanel, GamePanel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void disableAll()
+    {
+        homePanel.SetActive(false);
+        forYouPanel.SetActive(false);  
+        GamePanel.SetActive(false);
+        PostAQuestionPanel.SetActive(false);
+    }
+    public void enableHome()
+    {
+        disableAll();
+        homePanel.SetActive(true);
+    }
+    public void enableForYou()
+    {
+        disableAll();
+        forYouPanel.SetActive(true);
+    }
+    public void enablePosting()
+    {
+        disableAll();
+        PostAQuestionPanel.SetActive(true);
+    }
+    public void enableGame()
+    {
+        disableAll();
+        GamePanel.SetActive(true);
+    }
+}
